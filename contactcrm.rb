aaa334b91ad01ce5b5b7e07d46af4f8ffc5ada43
kevinberg3 <kevinberg3@gmail.com>
2013-06-07 01:23:16 +0000

    second commit with updates

--- contactcrm.rb
+++ contactcrm.rb
@@ -1,41 +1,49 @@
-#
-
-require_relative "databasecrm.rb"
+require_relative "databasecrm"
 
 class Contact
 
 attr_accessor :id, :firstname, :lastname, :email, :notes
 
 
-	def initialize
-		@contact = {
-		:@id => id_number,
-		:@firstname => "",
-		:@lastname =>  "",
-		:@email => "",
-		:@notes => ""
-	}
-
+	def initialize(id, firstname, lastname, email, notes)
+		@id = id
+		@firstname = firstname
+		@lastname =  lastname
+		@email = email
+		@notes = notes
 	end
+#initializing the values within the Contact class to be used to display the relevant contact information
+
+	def display_contact
+		puts "ID:             #{@id}"
+		puts "First Name:     #{@firstname}" 
+		puts "Last Name:      #{@lastname}"
+		puts "Email Address:  #{@email}"
+		puts "Notes on User:  #{@notes}"  
+	end
+#printing display contact to terminal
+		
+end
 
 
-	def valid_contact(contact)
-		# begin
-		if @contact.include? nil
-			#send back to initial puts query to collect @contact
-		else
-			create_contact
-		 # rescue Exception => e??
+# 	# def valid_contact(contact)
+# 	# 	# begin
+# 	# 	if @contact.include? nil
+# 	# 		#send back to initial puts query to collect @contact
+# 	# 	else
+# 	# 		create_contact
+# 	# 	 # rescue Exception => e??
 		 	
-		 end 
+# 	# 	 end 
 			
 
 
-	def create_contact(input)
+# 	# def create_contact(input)
+
 
 	
-	def display_all_contact
+# 	# def display_all_contact
 
-	def id_number
-		
+# 	# def id_number
+
 
--- crm_runner.rb
+++ crm_runner.rb
@@ -1,66 +1,96 @@
 #Runner
-require_relative "contactcrm.rb"
-require_relative "databasecrm.rb"
+# require_relative "./contactcrm.rb"
+require_relative 'databasecrm'
 
-class Crm_runner
+db = Database.new
+id = 100000
 
-def intialize 
-	@contact = []
-	def self.run
-		@crm = crm.new()
-		
+	puts "**********************************************************************"
+	puts "\n#{"********************Welcome to CRM*****************************"}\n\n"
+	puts "\n**********************************************************************"		
+
+	loop do	
+			
 		puts "\n#{"Please enter in add / modify / display all / display contact / display attribute / delete / exit".center(100)}\n\n"
-		gets.chomp.downcase.to_s = first_response
+		firstresponse = gets.chomp.downcase.to_s 
 
-		when first_response.include? "add"
-			puts "Enter first name"
-			first_name = gets.chomp.downcase.to_S 
-			puts "Enter last name"
-			last_name = gets.chomp.downcase.to_s 
-			puts "Enter email address"
-			email_address = gets.chomp.downcase.to_s 
-			puts "Enter any notes"
+		case firstresponse
+		when "add"
+			puts "Please enter first name"
+			firstname = gets.chomp.downcase.to_s
+			puts "Please enter last name"
+			lastname = gets.chomp.downcase.to_s 
+			puts "Please enter email address"
+			email = gets.chomp.downcase.to_s 
+			puts "Please enter any notes"
 			notes = gets.chomp.downcase.to_s 
+		
+			db.add(id, firstname, lastname, email, notes)
+			id += 1
 
-			contact.new << first_name << last_name << email_address << notes
+	
+		when "modify"
+			puts "\n#{Select attribute you wish to modify (first name, last name, email, notes}\n"
 
-				
-		when first_response == "modify"
-			 modify_contact
+			when "first name"
 
-		when first_response == "display all"
-			
-			#puts @contact??
-			#need to figure out how to access all contacts and print them
 
-		when first_response == "display contact"
-			display_particular_contact # need to include function that allows to find contact
-			
-		when first_response == "display attribute"
-			# need to include function that allows to find contact then sort by attribute 
+		when "display all"
+			puts contact #Doesn't seem to be correct
 
-		when first_response == "delete"
-			#find contact and how to remove contact
+		when "display contact"
+			puts "Please enter in attribute of the customer you wish to find"
+			keyword = gets.chomp.downcase.to_s
 
-		when first_response == "exit"
-			#figure out how to exit command line
+		when "display attribute"
 
-		else
-			# raise exception
 
-		end
+		when "delete"
 
-def search
-	puts ""
+		when "exit"
+# 			#figure out how to exit command line
 
+# 		else
+# 			# raise exception
+	end
 end
-		# 		while true
-		# 	puts "#{@game.board}\n\n"
-		# 	puts "\nPlayer #{@game.player_turn}: Please take your turn \n"
-		# 	number = gets.chomp.to_i
-		# 	@game.(number)       #turn(number)
-		# 	#@game.did_player_win?
-		# end
 
 
-crm_runner.run
+puts @contact
+
+
+# 		when "modify"
+# 			 modify_contact
+
+# 		when "display all"
+			
+# 			#puts @contact??
+# 			#need to figure out how to access all contacts and print them
+
+# 		when "display contact"
+# 			display_particular_contact # need to include function that allows to find contact
+			
+# 		when "display attribute"
+# 			# need to include function that allows to find contact then sort by attribute 
+
+# 		when "delete"
+# 			#find contact and how to remove contact
+
+
+
+# 		end
+
+# def search
+# 	puts ""
+
+# end
+# 		# 		while true
+# 		# 	puts "#{@game.board}\n\n"
+# 		# 	puts "\nPlayer #{@game.player_turn}: Please take your turn \n"
+# 		# 	number = gets.chomp.to_i
+# 		# 	@game.(number)       #turn(number)
+# 		# 	#@game.did_player_win?
+# 		# end
+
+
+# crm_runner.run
